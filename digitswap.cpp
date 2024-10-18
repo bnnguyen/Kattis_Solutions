f3739c309d463374670f3b8640fadb6c1c785287
Britney Nguyen <britney.nguyen249@gmail.com>
2024-10-18 18:16:34 -0400

    Update digitswap.cpp

--- digitswap.cpp
+++ digitswap.cpp
@@ -1,1 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+const ll N = 2e5+10;
+//int t;
+void solve()
+{
+    char a,b;
+    cin>>a>>b;
+    cout<<b<<a;
+}
 
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    solve();
+}
