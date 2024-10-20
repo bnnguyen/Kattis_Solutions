526934569ce65225e26039bab09f76144e7457d3
Britney Nguyen <britney.nguyen249@gmail.com>
2024-10-20 00:59:49 -0400

    Update filip.cpp

--- filip.cpp
+++ filip.cpp
@@ -1,1 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+const ll N = 2e5+10;
+//int t;
+void solve()
+{
+    string a,b;
+    cin>>a>>b;
+    reverse(a.begin(),a.end());
+    reverse(b.begin(),b.end());
+    cout<<max(a,b);
+}
 
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    solve();
+}
