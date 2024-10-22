135f6f2aad11da83237527a60be3def651455f13
Britney Nguyen <britney.nguyen249@gmail.com>
2024-10-21 22:31:11 -0400

    Update findingana.cpp

--- findingana.cpp
+++ findingana.cpp
@@ -1,1 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+const ll N = 2e5+10;
+//int t;
 
+void solve()
+{
+    string s;
+    cin>>s;
+    bool f=0;
+    for (char c:s)
+    {
+        if (c=='a') f=1;
+        if (f) cout<<c;
+    }
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+//    cin>>t;
+//    cin.ignore();
+//    while (t--)
+//    {
+//        solve();
+//    }
+    solve();
+    return 0;
+}
