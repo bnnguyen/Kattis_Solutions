86c361047b08ab262d2259de997f4933ff6a57af
Britney Nguyen <britney.nguyen249@gmail.com>
2024-10-27 22:23:07 -0400

    Update throwns.cpp

--- throwns.cpp
+++ throwns.cpp
@@ -1,1 +1,45 @@
-
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+const ll N = 110;
+int n,k;
+void solve()
+{
+    int ans=0;
+    cin>>n>>k;
+    vector<int> a;
+    a.push_back(0);
+    while (k--)
+    {
+        string u;
+        int v;
+        cin>>u;
+        //cout<<u<<' ';
+        if (u=="undo")
+        {
+            cin>>u;
+            v=stoi(u);
+            while (v--)
+            {
+                ans-=a[a.size()-1];
+                a.pop_back();
+            }
+            continue;
+        }
+        else
+        {
+            v=stoi(u);
+            a.push_back(v);
+            ans+=v;
+        }
+    }
+    if (ans>0) cout<<ans%n;
+    else cout<<(n-(-ans)%n)%n;
+}
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    solve();
+    return 0;
+}
