4f4ee0bbc06864f23d088b647192c3d09ee351d8
Britney Nguyen <britney.nguyen249@gmail.com>
2025-01-06 23:46:14 -0800

    Update fizzbuzz.cpp

--- fizzbuzz.cpp
+++ fizzbuzz.cpp
@@ -1,1 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+const ll N = 2e5+10;
+//int t;
+void solve()
+{
+    int x,y,n;
+    cin>>x>>y>>n;
+    for (int i=1;i<=n;i++)
+    if (i%x!=0&&i%y!=0) cout<<i<<'\n';
+    else
+    {
+        if (i%x==0) cout<<"Fizz";
+        if (i%y==0) cout<<"Buzz";
+        cout<<'\n';
+    }
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    solve();
+}
 
