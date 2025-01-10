f0a3a4504377561629492f0695ace5fa7f8828f4
Britney Nguyen <britney.nguyen249@gmail.com>
2025-01-09 23:10:17 -0800

    Update reduplikation.cpp

--- reduplikation.cpp
+++ reduplikation.cpp
@@ -1,1 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+const ll N = 2e5+10;
+//int t;
 
+void solve()
+{
+    string s;
+    cin>>s;
+    int n;
+    cin>>n;
+    while (n--) cout<<s;
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+//    cin>>t;
+//    cin.ignore();
+//    while (t--)
+//    {
+//        solve();
+//    }
+    solve();
+    return 0;
+}
