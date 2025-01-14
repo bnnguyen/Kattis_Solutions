c83cdc8afaa0dadc4fed215a3fa36f6d9742d7b4
Britney Nguyen <britney.nguyen249@gmail.com>
2025-01-13 22:12:23 -0500

    Update timeloop.cpp

--- timeloop.cpp
+++ timeloop.cpp
@@ -1,1 +1,14 @@
-
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+const ll N = 2e5+10;
+int n;
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    cin>>n;
+    for (int i=1;i<=n;i++)
+        cout<<i<<" Abracadabra\n";
+    return 0;
+}
