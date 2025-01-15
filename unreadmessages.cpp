4d3fd505f33a2b7a4c4a990734069c426ff40a23
Britney Nguyen <britney.nguyen249@gmail.com>
2025-01-14 22:14:27 -0500

    Update unreadmessages.cpp

--- unreadmessages.cpp
+++ unreadmessages.cpp
@@ -1,1 +1,29 @@
+#include <bits/stdc++.h>
+#define ll long long
+#define int ll
+#define ii pair<ll,ll>
 
+using namespace std;
+int n,m;
+unordered_map<int,int> h;
+
+void solve()
+{
+    cin>>n>>m;
+    for (int v,k=1; k<=m; k++)
+    {
+        cin>>v;
+        h[v]=k;
+        ll res = n-h.size();
+        res*=k;
+        for (auto it:h) res+=k-it.second;
+        cout<<res<<'\n';
+    }
+}
+
+signed main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    solve();
+}
